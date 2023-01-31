f4e13b60f5e02c1fd835aa7909652c8c5060efd4
piotr-ratajczak-pc <piotr.ratajczak@pragmaticcoders.com>
2023-01-31 11:46:36 +0100

    tut part 7

--- src/components/layout.js
+++ src/components/layout.js
@@ -20,8 +20,6 @@ const Layout = ({ pageTitle, children }) => {
     }
   `)
 
-  console.log(data)
-
   return (
     <div className={container}>
       <header className={siteTitle}>{data.site.siteMetadata.title}</header>
